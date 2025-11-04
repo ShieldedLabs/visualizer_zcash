37713860082ad9e4a513901c74d2fe8cb159d528
Sam H. Smith <sam.henning.smith@protonmail.com>
2025-11-04 02:09:23 +0100

    more text

--- src/other_file.rs
+++ src/other_file.rs
@@ -8,7 +8,7 @@ pub fn demo_of_rendering_stuff_with_context_that_allocates_in_the_background(dra
     let first_text = "This text is to the left.";
     let second_text = "This text is to the right.";
     let offset = draw_measure_text_line(draw_ctx, 16, first_text);
-    draw_text_line(draw_ctx, draw_ctx.window_width/2 + offset, draw_ctx.window_height/2, 16, first_text, 0xff0000);
+    draw_text_line(draw_ctx, draw_ctx.window_width/2 + offset, draw_ctx.window_height/2, 16, second_text, 0xff0000);
     draw_text_line(draw_ctx, draw_ctx.window_width/2, draw_ctx.window_height/2, 16, first_text, 0xff);
 
 }
